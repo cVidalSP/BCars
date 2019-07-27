6bc981ca857d94a97ceb11adffae7f2eee2a0a39
cVidal <kainan.vidal@gmail.com>
2019-07-27 11:57:52 -0300

    first commit

---
+++ test-rv/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import RvItems from './components/rvItems';
+
+function App() {
+  return (
+    <div>
+      <RvItems />
+    </div>
+  );
+}
+
+export default App;
---
+++ test-rv/src/services/axios.service.js
@@ -0,0 +1,7 @@
+import * as axios from 'axios';
+
+const api = axios.create({
+    baseURL : 'https://next.json-generator.com/api/json/get/41ORKNZDU'
+});
+
+export default api;
